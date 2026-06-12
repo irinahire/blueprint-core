2044c2b952fb932773e9e5f0d6c967c05d49fbc9
BlueLab <irinahire.ia@gmail.com>
2026-06-12 20:51:01 -0300

    Update motor-tests.js

--- apply/motor-tests.js
+++ apply/motor-tests.js
@@ -1,46 +1,37 @@
 const motorTests = {
     actual: 0,
-    ejercicios: [
-        {
-            titulo: "Escenario 1: Razonamiento Abstracto",
-            url: "https://www.bluelab.online/apply/test_a1.png",
-            pregunta: "¿Qué figura completa lógicamente el lugar del signo de interrogación?",
-            opciones: [
-                "url_opcion_1.png", "url_opcion_2.png", "url_opcion_3.png", "url_opcion_4.png",
-                "url_opcion_5.png", "url_opcion_6.png", "url_opcion_7.png", "url_opcion_8.png"
-            ]
-        }
-    ],
+    ejercicios: [{
+        titulo: "Escenario 1: Razonamiento Abstracto",
+        url: "https://www.bluelab.online/apply/test_a1.png",
+        pregunta: "¿Qué figura completa lógicamente el lugar del signo de interrogación?"
+    }],
 
     renderizar: function() {
-        console.log("Renderizando ejercicio:", this.actual + 1);
+        console.log("Renderizando...");
         const ej = this.ejercicios[this.actual];
         document.getElementById('titulo-escenario').innerText = ej.titulo;
         document.getElementById('pregunta-texto').innerText = ej.pregunta;
+        document.getElementById('matriz-img').src = ej.url;
         
-        const contenedor = document.getElementById('matriz-container');
-        contenedor.innerHTML = `<img src="${ej.url}" style="max-width: 400px;">`;
-        
-        const opcContainer = document.getElementById('opciones-container');
-        opcContainer.innerHTML = ej.opciones.map((url, index) => 
-            `<div class="opcion-btn" onclick="motorTests.seleccionar(${index})">
-                <img src="${url}" width="80px">
-            </div>`
-        ).join('');
-    },
-
-    seleccionar: function(index) {
-        console.log("Opción seleccionada:", index);
-        alert("Seleccionaste la opción: " + (index + 1));
+        const mapa = document.getElementById('mapa-opciones');
+        mapa.innerHTML = '';
+        // Creamos 8 áreas clicables sobre la imagen
+        for(let i = 1; i <= 8; i++) {
+            const div = document.createElement('div');
+            div.className = 'opcion-area';
+            div.onclick = () => {
+                console.log("Opción elegida:", i);
+                alert("Seleccionaste la opción " + i);
+            };
+            mapa.appendChild(div);
+        }
     },
 
     siguiente: function() {
-        if (this.actual < this.ejercicios.length - 1) {
-            this.actual++;
-            this.renderizar();
-        } else {
-            alert("¡Test finalizado!");
-        }
+        console.log("Avanzando...");
+        this.actual++;
+        if(this.actual < this.ejercicios.length) this.renderizar();
+        else alert("Fin del test");
     }
 };
 
