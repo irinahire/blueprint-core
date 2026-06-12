ba41a21a82987d43771e63b77fbea81ac4793b24
BlueLab <irinahire.ia@gmail.com>
2026-06-12 20:53:11 -0300

    Update motor-tests.js

--- apply/motor-tests.js
+++ apply/motor-tests.js
@@ -1,37 +1,27 @@
 const motorTests = {
     actual: 0,
-    ejercicios: [{
-        titulo: "Escenario 1: Razonamiento Abstracto",
-        url: "https://www.bluelab.online/apply/test_a1.png",
-        pregunta: "¿Qué figura completa lógicamente el lugar del signo de interrogación?"
-    }],
+    ejercicios: [
+        {
+            url: "https://www.bluelab.online/apply/test_a1.png"
+        }
+    ],
 
     renderizar: function() {
-        console.log("Renderizando...");
+        const contenedor = document.getElementById('matriz-container');
+        if (!contenedor) return; // Si no encuentra el div, no hace nada
+
         const ej = this.ejercicios[this.actual];
-        document.getElementById('titulo-escenario').innerText = ej.titulo;
-        document.getElementById('pregunta-texto').innerText = ej.pregunta;
-        document.getElementById('matriz-img').src = ej.url;
-        
-        const mapa = document.getElementById('mapa-opciones');
-        mapa.innerHTML = '';
-        // Creamos 8 áreas clicables sobre la imagen
-        for(let i = 1; i <= 8; i++) {
-            const div = document.createElement('div');
-            div.className = 'opcion-area';
-            div.onclick = () => {
-                console.log("Opción elegida:", i);
-                alert("Seleccionaste la opción " + i);
-            };
-            mapa.appendChild(div);
-        }
+        contenedor.innerHTML = `<img src="${ej.url}" style="width: 100%; grid-column: span 3;">`;
+        console.log("Renderizado correctamente");
     },
 
     siguiente: function() {
-        console.log("Avanzando...");
-        this.actual++;
-        if(this.actual < this.ejercicios.length) this.renderizar();
-        else alert("Fin del test");
+        if (this.actual < this.ejercicios.length - 1) {
+            this.actual++;
+            this.renderizar();
+        } else {
+            alert("Has finalizado todos los ejercicios.");
+        }
     }
 };
 
