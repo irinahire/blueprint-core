b5c534d824aa9481610e29e5d05d86b1aaf878b5
BlueLab <irinahire.ia@gmail.com>
2026-06-12 21:12:53 -0300

    Update motor-tests.js

--- apply/motor-tests.js
+++ apply/motor-tests.js
@@ -2,7 +2,6 @@ const motorTests = {
     actual: 0,
     ejercicios: [
         { url: "https://www.bluelab.online/apply/test_a1.png" }
-        // Aquí podés ir agregando más objetos { url: "..." }
     ],
 
     init: function() {
@@ -11,37 +10,25 @@ const motorTests = {
     },
 
     renderizar: function() {
-        const ej = this.ejercicios[this.actual];
-        const img = document.getElementById('test-image');
         const overlay = document.getElementById('click-overlay');
-        const numEscenario = document.getElementById('num-escenario');
-
-        if (img) img.src = ej.url;
-        if (numEscenario) numEscenario.innerText = this.actual + 1;
-
-        if (overlay) {
-            overlay.innerHTML = '';
-            for (let i = 1; i <= 8; i++) {
-                const div = document.createElement('div');
-                div.className = 'clickable-area';
-                div.onclick = () => this.seleccionar(i);
-                overlay.appendChild(div);
-            }
+        if (!overlay) return;
+        
+        overlay.innerHTML = '';
+        for (let i = 1; i <= 8; i++) {
+            const div = document.createElement('div');
+            div.className = 'clickable-area';
+            div.onclick = () => this.seleccionar(i);
+            overlay.appendChild(div);
         }
     },
 
     seleccionar: function(opcion) {
-        console.log("Opción seleccionada:", opcion);
-        alert("Seleccionaste la opción " + opcion);
+        console.log("Respuesta capturada:", opcion);
+        alert("Opción seleccionada: " + opcion);
     },
 
     siguiente: function() {
-        if (this.actual < this.ejercicios.length - 1) {
-            this.actual++;
-            this.renderizar();
-        } else {
-            alert("Has finalizado todos los ejercicios.");
-        }
+        alert("Avanzando al próximo ejercicio...");
     }
 };
 
