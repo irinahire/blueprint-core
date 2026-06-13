7d6c9e8c513669ee16133393197aa46a15149a23
BlueLab <irinahire.ia@gmail.com>
2026-06-12 21:08:37 -0300

    Update motor-tests.js

--- apply/motor-tests.js
+++ apply/motor-tests.js
@@ -1,8 +1,8 @@
 const motorTests = {
     actual: 0,
     ejercicios: [
-        { url: "https://www.bluelab.online/apply/test_a1.png" },
-        // Agrega aquí los próximos: { url: "url_ejercicio_2.png" }, etc.
+        { url: "https://www.bluelab.online/apply/test_a1.png" }
+        // Aquí podés ir agregando más objetos { url: "..." }
     ],
 
     init: function() {
@@ -12,23 +12,27 @@ const motorTests = {
 
     renderizar: function() {
         const ej = this.ejercicios[this.actual];
-        document.getElementById('num-escenario').innerText = this.actual + 1;
-        document.getElementById('test-image').src = ej.url;
-        
+        const img = document.getElementById('test-image');
         const overlay = document.getElementById('click-overlay');
-        overlay.innerHTML = '';
-        
-        for (let i = 1; i <= 8; i++) {
-            const div = document.createElement('div');
-            div.className = 'clickable-area';
-            div.onclick = () => this.seleccionar(i);
-            overlay.appendChild(div);
+        const numEscenario = document.getElementById('num-escenario');
+
+        if (img) img.src = ej.url;
+        if (numEscenario) numEscenario.innerText = this.actual + 1;
+
+        if (overlay) {
+            overlay.innerHTML = '';
+            for (let i = 1; i <= 8; i++) {
+                const div = document.createElement('div');
+                div.className = 'clickable-area';
+                div.onclick = () => this.seleccionar(i);
+                overlay.appendChild(div);
+            }
         }
     },
 
     seleccionar: function(opcion) {
-        console.log("Usuario seleccionó opción:", opcion);
-        alert("Opción " + opcion + " registrada en el sistema.");
+        console.log("Opción seleccionada:", opcion);
+        alert("Seleccionaste la opción " + opcion);
     },
 
     siguiente: function() {
@@ -36,7 +40,7 @@ const motorTests = {
             this.actual++;
             this.renderizar();
         } else {
-            alert("Has finalizado todos los ejercicios del test.");
+            alert("Has finalizado todos los ejercicios.");
         }
     }
 };
