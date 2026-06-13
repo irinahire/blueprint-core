19b48f16320962ea357e12019bb41ada628ff60a
BlueLab <irinahire.ia@gmail.com>
2026-06-12 21:33:00 -0300

    Update motor-tests.js

--- apply/motor-tests.js
+++ apply/motor-tests.js
@@ -1,34 +1,31 @@
 const motorTests = {
     actual: 0,
-    ejercicios: [
-        { url: "https://www.bluelab.online/apply/test_a1.png" }
-    ],
+    seleccion: null,
 
     init: function() {
-        console.log("Motor inicializado");
-        this.renderizar();
-    },
-
-    renderizar: function() {
         const overlay = document.getElementById('click-overlay');
-        if (!overlay) return;
-        
         overlay.innerHTML = '';
         for (let i = 1; i <= 8; i++) {
             const div = document.createElement('div');
             div.className = 'clickable-area';
-            div.onclick = () => this.seleccionar(i);
+            div.onclick = () => this.registrar(i);
             overlay.appendChild(div);
         }
     },
 
-    seleccionar: function(opcion) {
-        console.log("Respuesta capturada:", opcion);
-        alert("Opción seleccionada: " + opcion);
+    registrar: function(opcion) {
+        this.seleccion = opcion;
+        const box = document.getElementById('feedback-box');
+        const text = document.getElementById('msg-text');
+        
+        box.style.display = 'block';
+        text.innerText = "Has seleccionado la opción " + opcion + ".";
+        console.log("Respuesta capturada sin popup:", opcion);
     },
 
     siguiente: function() {
-        alert("Avanzando al próximo ejercicio...");
+        alert("Avanzando al siguiente nivel...");
+        // Aquí podrías cargar la siguiente imagen o redirigir
     }
 };
 
